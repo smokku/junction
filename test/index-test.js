db0230ea95c8c7846583c8c6b18ac8f373f3d752
Tomasz Sterna <smoku@dcclabs.tv>
2017-06-13 11:58:11 +0200

    Support working under browser

--- test/index-test.js
+++ test/index-test.js
@@ -6,10 +6,6 @@ var util = require('util');
 
 vows.describe('junction').addBatch({
   
-  'should export version': function() {
-    assert.isString(junction.version);
-  },
-  
   'should export create function': function() {
     assert.isFunction(junction);
     assert.isFunction(junction.create);
@@ -20,14 +16,6 @@ vows.describe('junction').addBatch({
     assert.isFunction(junction.JID);
     assert.isFunction(junction.XMLElement);
     assert.isFunction(junction.XMPPStanza);
-  },
-  
-  'should export middleware': function() {
-    assert.isObject(junction.middleware);
-  },
-  
-  'should export filters': function() {
-    assert.isObject(junction.filters);
   },
   
   'should export errors': function() {
